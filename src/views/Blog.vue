335150ce31fbf010dff583ac0defe63951de49fd
charmingsong <web_songsong@163.com>
2018-10-08 22:35:05 +0800

    vue cli3 创建stylus 的全局方法和变量

---
+++ src/views/Blog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="Blog">
+    this Blog
+    <div class="test"></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {}
+}
+</script>
+<style lang="stylus" scoped>
+.test
+  width $width
+  height r(300)
+  background red
+</style>
