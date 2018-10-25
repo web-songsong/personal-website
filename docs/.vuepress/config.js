c1986e84cd37bf3d6d963ca128d06e774db05e99
charmingsong <web_songsong@163.com>
2018-10-25 16:21:16 +0800

    vuepress--> config.js，侧边栏，导航栏优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 const path = require('path')
 module.exports = {
   dest: '../blog',
-  // base: '/blog/',
   title: 'charmingsong blog',
   description: '会做前端和做好前端是两回事',
   head: [
@@ -18,41 +17,97 @@ module.exports = {
     ]
   ],
   themeConfig: {
-    nav: navFn(path.join(__dirname, '../')),
-    displayAllHeaders: true,
-    sidebar: 'auto'
+    nav: [
+      ...navInit(path.join(__dirname, '../'), 'docs'),
+      { text: 'GitHub', link: 'https://github.com/web-songsong' }
+    ],
+    sidebar: sidebar(path.join(__dirname, '../'))
   }
 }
-function navFn(pwd) {
-  const arr = []
-  fs.readdirSync(pwd)
-    .map(item => {
-      const item_pwd = path.join(pwd, item)
-      if (/^[^.]/.test(item)) {
-        if (fs.statSync(item_pwd).isDirectory()) {
-          return {
-            // text: `${item.replace(/(\.\w*)/g, '')}`,
-            text: item,
-            items: navFn(item_pwd)
-          }
-        } else {
-          return {
-            text: `${
-              item === 'README.md' ? 'home' : item.replace(/(\.\w*)/g, '')
-            }`,
-            link: `/${
-              item !== 'README.md'
-                ? item_pwd
-                    .replace(path.join(__dirname, '../'), '')
-                    .replace(/(\.\w*)/g, '.html')
-                : item_pwd
-                    .replace(path.join(__dirname, '../'), '')
-                    .replace('README.md', '')
-            }`
-          }
-        }
+/**
+ *config配置文件的自动后去配置参数
+ *
+ * @param {*} dir 文件路径
+ * @param {*} filename 文件夹的名字
+ * @param {string} [link=''] 配置文件中的链接
+ * @returns array
+ */
+function navInit(dir, filename, link = '') {
+  const navs = []
+  fs.readdirSync(dir).forEach(item => {
+    if (!/^[^.]/.test(item)) return
+    if (fs.statSync(path.join(dir, item)).isDirectory()) {
+      if (dirflag(path.join(dir, item))) {
+        navs.push({
+          text: item,
+          items: navInit(path.join(dir, item), item, `${link}/${item}`)
+        })
+      } else {
+        navs.push({
+          text: item,
+          link: `${link}/${item}/`
+        })
       }
-    })
-    .forEach(item => (item ? arr.push(item) : ''))
-  return arr
+    }
+  })
+  return navs
+}
+
+/**
+ *sidebar的自动配置参数
+ *
+ * @param {*} dir 路径
+ * @param {string} [urlname=''] 链接的名字
+ * @returns Object
+ */
+function sidebar(dir, urlname = '') {
+  const side = {}
+  fs.readdirSync(dir).forEach(item => {
+    if (!/^[^.]/.test(item)) return
+    if (fs.statSync(path.join(dir, item)).isDirectory()) {
+      if (dirflag(path.join(dir, item))) {
+        Object.assign(side, sidebar(path.join(dir, item), `${urlname}/${item}`))
+      } else {
+        Object.assign(side, dirlink(path.join(dir, item), `${urlname}/${item}`))
+      }
+    }
+  })
+  Object.values(side).forEach(arr => arr.sort())
+  return side
+}
+
+/**
+ * 文件过滤，对路径中的只有文件的文件夹进行参数输出
+ *
+ * @param {*} file 文件路径
+ * @param {*} filename 文件名
+ * @returns Object
+ */
+function dirlink(file, filename) {
+  const sidebar = {}
+  fs.readdirSync(file).forEach(item => {
+    let url = ''
+    switch (item) {
+      case 'README.md':
+        url = ''
+        break
+      default:
+        url = item.replace('.md', '')
+    }
+    sidebar[`${filename}/`]
+      ? sidebar[`${filename}/`].push(url)
+      : (sidebar[`${filename}/`] = [url])
+  })
+  return sidebar
+}
+/**
+ *检查文件中是否有文件夹
+ *
+ * @param {*} filedir-文件路径
+ * @returns true/false
+ */
+function dirflag(filedir) {
+  return fs
+    .readdirSync(filedir)
+    .some(item => fs.statSync(path.join(filedir, item)).isDirectory())
 }
---
+++ docs/tool/song-vue-template/README.md
@@ -0,0 +1,27 @@
+# song-vue-template
+vue 组件
+
+## 安装
+```
+yarn add -D song-vue-template
+```
+
+### 在vue中使用组件
+
+1. 在main.js文件中写入
+
+``` javascript
+import Svt from 'song-vue-template'
+import 'song-vue-template/dist/svt.css'
+Vue.use(Svt)
+
+```
+
+2. 在开发的vue页面中使用
+```
+<svt-template/>
+```
+
+## 组件文档
+
+### ☞ [homepage](https://blog.websong.xin/tool/song-vue-template/)
