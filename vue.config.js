eba28a50cd7d127c8f34b8d8c9b8bae86a928072
charmingsong <web_songsong@163.com>
2018-11-29 11:21:15 +0800

    seo

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const copyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
   chainWebpack: config => {
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
@@ -6,7 +7,18 @@ module.exports = {
       addStyleResource(config.module.rule('stylus').oneOf(type))
     )
   },
-  outputDir: '../home'
+  outputDir: '../home',
+  configureWebpack: {
+    plugins: [
+      new copyWebpackPlugin([
+        {
+          from: path.join(__dirname, './baidu_verify_lBiznfiSA0.html'),
+          to: path.join(__dirname, '../home'),
+          ignore: ['.*']
+        }
+      ])
+    ]
+  }
 }
 // 定义全局的stylus方法和变量
 function addStyleResource(rule) {
