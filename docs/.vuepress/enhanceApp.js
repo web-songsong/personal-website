65297c80717a64a56fccc2cbaffbc71f3f15d9e5
charmingsong <web_songsong@163.com>
2019-01-17 01:56:39 +0800

    update

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,5 +1,5 @@
 import Svt from 'song-vue-template'
-import 'song-vue-template/dist/svt.css'
+// import 'song-vue-template/dist/svt.css'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
