1fe623c1cd542542c490d560a0dbd726181a9ead
charming song <web_songsong@163.com>
2019-03-19 00:56:42 +0800

    Update enhanceApp.js

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,5 +1,5 @@
 import Svt from 'song-vue-template'
-import 'song-vue-template/dist/svt.css'
+import 'song-vue-template/dist/css/svt.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 export default ({ Vue }) => {
