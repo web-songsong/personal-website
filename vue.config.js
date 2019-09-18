6c9d296db306d166f92b9213b3f56ef779d45b8d
charmingsong <web_songsong@163.com>
2019-09-18 19:50:20 +0800

    111

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const isProduction = process.env.NODE_ENV === 'production'
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
 const nodeExternals = require('webpack-node-externals')
@@ -10,26 +12,41 @@ const target = TARGET_NODE ? 'server' : 'client'
 const resolve = dir => path.join(__dirname, dir)
 
 module.exports = {
-  css: {
-    extract: false
-  },
-  configureWebpack: () => ({
-    entry: `./src/entry-${target}.js`,
-
-    devtool: 'source-map',
-    target: TARGET_NODE ? 'node' : 'web',
-    node: false,
-    output: {
-      libraryTarget: TARGET_NODE ? 'commonjs2' : undefined
-    },
-
-    externals: TARGET_NODE
-      ? nodeExternals({
-          whitelist: [/\.css$/]
+  css: {},
+  configureWebpack: config => {
+    let plugins = [
+      TARGET_NODE ? new VueSSRServerPlugin() : new VueSSRClientPlugin()
+    ]
+    if (isProduction) {
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          // 正在匹配需要压缩的文件后缀
+          test: /\.(js|css|svg|woff|ttf|json|html)$/,
+          // 大于10kb的会压缩
+          threshold: 10240,
+          // 其余配置查看compression-webpack-plugin
+          deleteOriginalAssets: false
         })
-      : undefined,
-    plugins: [TARGET_NODE ? new VueSSRServerPlugin() : new VueSSRClientPlugin()]
-  }),
+      )
+    }
+
+    return {
+      entry: `./src/entry-${target}.js`,
+      devtool: isProduction ? 'source-map' : 'none',
+      target: TARGET_NODE ? 'node' : 'web',
+      node: false,
+      output: {
+        libraryTarget: TARGET_NODE ? 'commonjs2' : undefined
+      },
+
+      externals: TARGET_NODE
+        ? nodeExternals({
+            whitelist: [/\.css$/]
+          })
+        : undefined,
+      plugins
+    }
+  },
   chainWebpack: config => {
     config.resolve.alias
       .set('utils', resolve('src/assets/utils/'))
@@ -45,5 +62,5 @@ module.exports = {
       })
   },
   filenameHashing: false,
-  indexPath: 'dev.html'
+  indexPath: 'spa'
 }
