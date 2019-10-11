0a1deeceb5251691e3152fad1af8099ab996c1d8
charmingsong <web_songsong@163.com>
2019-10-11 19:56:32 +0800

    update git .

---
+++ src/views/test/test-test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="test-test">
+    this websong.xin test vue.js
+    <div @click="handleTest">test button</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test-test',
+  methods: {
+    handleTest() {
+      console.log('asdf')
+    }
+  },
+  mounted() {
+    console.log('asdf')
+  }
+}
+</script>
+
+<style scoped>
+</style>
